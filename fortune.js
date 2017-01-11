0247dcfa01a444f89898ba8d3d754a86a7615513
Anthony Desormeau <anthony.desormeau@gmail.com>
2017-01-11 01:13:47 +0000

    Fortune function complte

--- fortune.js
+++ fortune.js
@@ -1,1 +1,22 @@
 // Fortune.js file
+var quotes = ["Those who fail to train, train to fail",
+"only those who wear shoes can walk", 
+"Where did my dog go?", 
+"Have you seen my grandson?", 
+"Magic is only real to the believer",
+"running can get you there faster, but you will miss the view",
+"rain is great when you need water",
+"Look out for that car!",
+"Mom! Where is my SNACK PACK",
+"Wait a second.... I think he's dead"];
+//
+/*
+(function greatQuotes(array){
+    return array[Math.floor(Math.random()*10)];
+}(quotes));*/
+
+function greatQuotes(array){
+    return array[Math.floor(Math.random()*10)];
+}
+
+console.log(greatQuotes(quotes));
